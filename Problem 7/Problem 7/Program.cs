a7e2b5a8f5ee3953d01ed39bae4f7998ce52b749
garyng <garyng.zhongbo@gmail.com>
2013-09-19 22:19:06 +0800

    Solve Problem#7

---
+++ Problem 7/Problem 7/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GenPrime(1000000)[10001-1]);
+            Console.ReadKey();
+        }
+
+        public static List<int> GenPrime(int max)
+        {
+            List<bool> bSieve = new List<bool>();
+            List<int> iNumbers = new List<int>();
+
+            for (int i = 1; i <= max; i++)  //exclude 1
+            {
+                bSieve.Add(true);  //true = prime
+                iNumbers.Add(i);
+            }
+            bSieve[0] = false;  //[0]=1
+            for (int i = 1; i < iNumbers.Count; i++)
+            {
+                for (int y = iNumbers[i]; y <= iNumbers.Last(); y += iNumbers[i])
+                {
+                    if (y == iNumbers[i]) continue;
+                    bSieve[y - 1] = false;
+                }
+            }
+            List<int> Prime = new List<int>();
+            for (int i = 0; i < bSieve.Count; i++)
+            {
+                if (bSieve[i] == true)
+                {
+                    Prime.Add(iNumbers[i]);
+                }
+            }
+
+            return Prime;
+        }
+
+    }
+}
