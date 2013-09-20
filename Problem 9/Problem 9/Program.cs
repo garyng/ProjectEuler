8dc9533b47ed60178a1129fce06e9037f40a1d42
garyng <garyng.zhongbo@gmail.com>
2013-09-20 11:10:14 +0800

    Solve Problem#9

---
+++ Problem 9/Problem 9/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = 1000;
+            int a = 0;
+            int b = 0;
+            int c = 0;
+            bool found = false;
+
+            for (a = 1; a < s; a++)
+            {
+                for (b = a; b < s; b++)
+                {
+                    c = s - a - b;
+                    if (a * a + b * b == c * c)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if (found)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(a + " " + b + " " + c + " " + a*b*c);
+            Console.ReadKey();
+        }
+    }
+}
