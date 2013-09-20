ec955a8ec9fe82d5dfd2364acac036e6e0752e71
garyng <garyng.zhongbo@gmail.com>
2013-09-20 09:21:14 +0800

    Solve Problem#20

---
+++ Problem 20/Problem 20/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+namespace Problem_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger fac = Factorial(100);
+            Console.WriteLine("100! : " + fac);
+            int sum = 0;
+            while (fac > 0)
+            {
+                sum += (int)(fac % 10);
+                fac /= 10;
+            }
+            Console.WriteLine("Sum : " + sum);
+            Console.ReadKey();
+        }
+
+        static BigInteger Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+        }
+    }
+}
