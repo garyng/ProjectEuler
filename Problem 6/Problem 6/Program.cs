e3db366a9318f82b266f725c1589a7129c5bc4ed
garyng <garyng.zhongbo@gmail.com>
2013-09-20 11:08:54 +0800

    Solve Problem#6

---
+++ Problem 6/Problem 6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Pow(SumOf1toN(100), 2) - SumOf1toNSquare(100));
+            Console.ReadKey();
+        }
+
+        static double SumOf1toN(double n)
+        {
+            // formula : ((1+n)/2)*n
+            return ((1 + n) / 2) * n;
+        }
+
+        static double SumOf1toNSquare(double n)
+        {
+            // formula : (n(2n+1)(n+1))/6
+            return (n * (2 * n + 1) * (n + 1)) / 6;
+        }
+    }
+}
