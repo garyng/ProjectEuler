26153d288d9ff24f3515d1cf2558d2073f21de9c
garyng <garyng.zhongbo@gmail.com>
2013-09-20 09:16:03 +0800

    Solve Problem#16

---
+++ Problem 16/Problem 16/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+namespace Problem_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger pow = BigInteger.Pow(2, 1000);
+            Console.WriteLine("2^1000 : " + pow);
+            int sum=0;
+            while (pow > 0)
+            {
+                sum += (int)(pow % 10);
+                pow /= 10;
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
