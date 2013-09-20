9ce7ec5e1c17cf9359c2109b72afce30f2975e8d
garyng <garyng.zhongbo@gmail.com>
2013-09-20 11:03:29 +0800

    Solve Problem#3

---
+++ Problem 3/Problem 3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Problem_3
+{
+    class Program
+    {
+        //What is the largest prime factor of the number 600851475143 ?
+        static void Main(string[] args)
+        {
+            List<int> Prime = ReadPrimeFromFile("Primes.txt");
+
+            TrialDiv(Prime, 600851475143);
+            Console.WriteLine("Done.");
+            Console.ReadKey();
+
+        }
+
+        static void TrialDiv(List<int> primes, double num)
+        {
+            // No optimization..
+            for (int i = 0; i < primes.Count; i++)
+            {
+                if (num % primes[i] == 0)
+                {
+                    Console.WriteLine(primes[i]); 
+                }
+            }
+        }
+
+        static List<int> ReadPrimeFromFile(string FileName)
+        {
+            StreamReader sr = new StreamReader(FileName);
+            List<int> iPrimes = new List<int>();
+            List<string> strPrimes = Regex.Split(sr.ReadToEnd(), Environment.NewLine).ToList();
+            //remove the last blank line
+            strPrimes.RemoveAt(strPrimes.Count - 1);
+            strPrimes.ForEach(item => iPrimes.Add(Convert.ToInt32(item)));
+            return iPrimes;
+        }
+    }
+}
