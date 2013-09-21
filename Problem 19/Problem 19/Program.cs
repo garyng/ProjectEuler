b52278efce37aeb66b40f2da0ea104647d246c4e
garyng <garyng.zhongbo@gmail.com>
2013-09-21 20:19:23 +0800

    Solve Problem#19

---
+++ Problem 19/Problem 19/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sundays = 0;
+            for (int year = 1901; year <= 2000; year++)
+            {
+                for (int month = 1; month <= 12; month++)
+                {
+                    if ((new DateTime(year,month,1)).DayOfWeek == DayOfWeek.Sunday)
+                    {
+                        sundays++;
+                        Console.WriteLine("{0}/{1}/{2} is Sunday",1,month,year);
+                    }
+                }
+            }
+            Console.WriteLine("Total : {0}",sundays);
+            Console.ReadKey();
+        }
+
+
+    }
+}
