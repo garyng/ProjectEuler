95c09197b12c18b3d8a034bb2b9b19cfe88ddf26
GaryNg <garyng.zhongbo@gmail.com>
2013-10-12 17:07:13 +0800

    Solve Problem#24

---
+++ Problem 24/Problem 24/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem_24
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> factorial = cacheFactorial(9);
+
+			List<int> number = new List<int>() { 0,1,2,3,4,5,6,7,8,9 };
+			List<int> result = new List<int>();
+
+			int nth = 1000000;
+			
+			//skip the last one
+			for (int i = factorial.Count - 1; i >= 0; i--)
+			{
+				for (int j = 0; j < number.Count; j++)
+				{
+					//Console.WriteLine(  (i+1) + " " + factorial[i] + " " + j + " " + factorial[i]*(j+1));
+					if ((factorial[i] * j +1) <= nth && factorial[i] * (j + 1) >= nth)
+					{
+						//Console.WriteLine(factorial[i] * j + 1 + "~" + factorial[i] * (j + 1));
+						//Console.WriteLine(factorial[i]*(j+1));
+						//Console.WriteLine(nth);
+						result.Add(number[j]);
+						number.RemoveAt(j);
+						nth -= factorial[i] * j;
+
+						break;
+					}
+				}
+			}
+
+			Console.WriteLine(string.Join("",result) + number[0]);
+			Console.ReadKey();
+
+		}
+
+		public static List<int> cacheFactorial(int n)
+		{
+			List<int> factorial = new List<int>();
+			int fac = 1;
+			for (int i = 1; i <= n; i++)
+			{
+				fac *= i;
+				factorial.Add(fac);
+			}
+			return factorial;
+		}
+
+		
+	}
+}
---
+++ Problem 24/Problem 24/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Problem 24")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GaryNg")]
+[assembly: AssemblyProduct("Problem 24")]
+[assembly: AssemblyCopyright("Copyright © GaryNg 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e0da57c9-d026-480c-9c2e-11b8aa970820")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
