5a9406d7e1c0784177a44f55a34fd608c98d6031
GaryNg <garyng.zhongbo@gmail.com>
2013-10-13 20:12:32 +0800

    Solve Problem#28

---
+++ Problem 28/Problem 28/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_28
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Console.WriteLine(calDiagSum((1001-1)/2));
+			Console.ReadKey();
+		}
+
+		private static int calDiagSum(int n)
+		{
+			if (n == 0)
+			{
+				return 1;
+			}
+			else
+			{
+				return (int)Math.Pow((2*n+1),2)*4 - 12 * n + calDiagSum(n - 1);
+			}
+		}
+	}
+}
