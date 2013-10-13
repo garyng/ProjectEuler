82a221eb121652473335ff2f3025248c1c68e882
GaryNg <garyng.zhongbo@gmail.com>
2013-10-13 11:58:16 +0800

    Solve problem#25

---
+++ Problem 25/Problem 25/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace Problem_25
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			BigInteger limit = BigInteger.Pow(10, 999);
+			BigInteger fibNum = BigInteger.Zero;
+			BigInteger prevFibNum = 1;
+			BigInteger prevFibNum1 = 1;
+			int count = 2;
+			while (fibNum < limit)
+			{
+				fibNum = prevFibNum + prevFibNum1;
+				count++;
+				prevFibNum1 = prevFibNum;
+				prevFibNum = fibNum;
+			}
+			Console.WriteLine("fib#" + count + " : \n" + fibNum);
+
+			Console.ReadKey();
+		}
+	}
+}
